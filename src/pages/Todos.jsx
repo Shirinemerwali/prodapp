ac1ae6ccab89cad8de7b5215017c2578a7545dec
AishaH <aisha_1@live.se>
2025-12-09 15:29:54 +0100

    Lade till Login och Register sidor, samt uppdaterade Todos komponenten

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,18 +1,13 @@
-
+import React, {useEffect} from 'react';
+import createItem from '../Models/Item';
 
 
 function Todos () {
 
 return (
     <div>
-    <h1> Todo page</h1>
-
+        <h1> Todo page</h1>
     </div>
-
-
 );
-
-
 }
-
 export default Todos ;
