eb5dc246228f1f5b9e25dda9f983cf06f3e7c51b
AishaH <aisha_1@live.se>
2025-12-10 21:42:55 +0100

    Uppdaterade Home-komponenten med inloggnings- och registreringsformulär samt tog bort onödiga rutter och Navbar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import Todos from "./pages/Todos";
-import Habits from "./pages/Habits";
-import Events from "./pages/Events";
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/todos" element={<Todos />} />
-        <Route path="/habits" element={<Habits />} />
-        <Route path="/events" element={<Events />} />
       </Routes>
     </Router>
   );
